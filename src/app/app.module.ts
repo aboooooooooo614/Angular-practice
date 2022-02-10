66d5e9fd4f822915b76ede405652675cfbaed491
aboooooooooo614 <kudamonotabe@gmail.com>
2022-02-10 14:58:30 +0800

    3 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ItemComponent],
+  declarations: [AppComponent, ItemComponent],
+  imports: [BrowserModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
